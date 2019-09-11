28d44b5e7d5713380166efe7a8cff2db0abac7b7
kaijun <kaijun@fit2cloud.com>
2019-09-11 18:34:51 +0800

    [update] 修改client可以修改请求头信息

--- src/main/java/com/jumpserver/sdk/v2/builder/JMSClient.java
+++ src/main/java/com/jumpserver/sdk/v2/builder/JMSClient.java
@@ -6,24 +6,57 @@ import com.jumpserver.sdk.v2.jumpserver.org.OrgService;
 import com.jumpserver.sdk.v2.jumpserver.permissions.PermissionService;
 import com.jumpserver.sdk.v2.jumpserver.users.UserService;
 
+import java.util.Map;
+
 public interface JMSClient {
 
-    //token 信息
+    /**
+     * token 信息
+     *
+     * @return
+     */
     Token getToken();
 
-    //users 用户信息
+    /**
+     * users 用户信息
+     *
+     * @return
+     */
     UserService users();
 
-    //assets 资产信息
+    /**
+     * assets 资产信息
+     *
+     * @return
+     */
     AssertsService assets();
 
-    //assets 资产信息
+    /**
+     * assets 资产信息
+     *
+     * @return
+     */
     OrgService orgs();
 
-    //luna 终端界面
+    /**
+     * luna 终端界面
+     *
+     * @return
+     */
     LunaService luna();
 
-    //permissions 授权规则
+    /**
+     * permissions 授权规则
+     *
+     * @return
+     */
     PermissionService permissions();
+
+    /**
+     * 获得请求头
+     *
+     * @return
+     */
+    Map getHeaders();
 
 }
--- src/test/java/client/JMSClientTest.java
+++ src/test/java/client/JMSClientTest.java
@@ -2,13 +2,16 @@ package client;
 
 import com.jumpserver.sdk.v2.builder.ClientBuilder;
 import com.jumpserver.sdk.v2.builder.JMSClient;
+import com.jumpserver.sdk.v2.common.ClientConstants;
 import com.jumpserver.sdk.v2.httpclient.build.Config;
+import com.jumpserver.sdk.v2.model.UserGroup;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -43,9 +46,9 @@ public class JMSClientTest {
         try {
             JMSClient os;
             ClientBuilder credentials = new ClientBuilder()
-                                            .endpoint(endPoint)
-                                            .credentials(username, password)
-                                            .withConfig(Config.newConfig().withConnectionTimeout(10000).withReadTimeout(10000));
+                    .endpoint(endPoint)
+                    .credentials(username, password)
+                    .withConfig(Config.newConfig().withConnectionTimeout(10000).withReadTimeout(10000));
             if (StringUtils.isBlank(orgId)) {
                 os = credentials.authenticate();
             } else {
@@ -62,4 +65,21 @@ public class JMSClientTest {
         }
     }
 
+    @Test
+    public void tokenWithOrgChange() {
+        JMSClient client = new ClientBuilder()
+                .endpoint(endPoint)
+                .credentials(username, password)
+                .header("x-jms-org","2107470f-1107-4352-84fc-8de6ef3b7fe8")
+                .withConfig(Config.newConfig().withConnectionTimeout(10000).withReadTimeout(10000)).authenticate();
+        System.out.println(client.getHeaders());
+        List<UserGroup> userGroups = client.users().userGroups();
+        userGroups.stream().forEach(u -> System.out.println(u.getName()));
+
+        //切换组织
+        client.getHeaders().put("x-jms-org", "84d8a23e-5bd9-410e-9438-756054daf128");
+        System.out.println(client.getHeaders());
+        List<UserGroup> userGroups2 = client.users().userGroups();
+        userGroups2.stream().forEach(u -> System.out.println(u.getName()));
+    }
 }
